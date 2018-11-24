df13f9267ba936ec4c3778704f2907327a4836fc
Nazmul Alam <nazmuldipu@gmail.com>
2018-11-24 17:43:26 +0600

    feature added

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -6,9 +6,9 @@ import { IndexComponent } from './containers/index/index.component';
 import { HomeComponent } from './home.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { IntoComponent } from './components/into/into.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { BaseInfoComponent } from './components/base-info/base-info.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 export const ROUTES: Routes = [
   {
@@ -26,6 +26,14 @@ export const ROUTES: Routes = [
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(ROUTES)],
-  declarations: [HomeComponent, IndexComponent, SideNavComponent, NavBarComponent, IntoComponent, IntroComponent, BaseInfoComponent]
+  declarations: [
+    HomeComponent,
+    IndexComponent,
+    SideNavComponent,
+    NavBarComponent,
+    IntroComponent,
+    BaseInfoComponent,
+    FeaturesComponent
+  ]
 })
 export class HomeModule {}
